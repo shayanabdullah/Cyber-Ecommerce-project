09eaa6bb931ac74acd775ab47ef505bf8853fe9f
shayanabdullah <tuhinafroza07@gmail.com>
2026-01-01 03:54:30 +0600

    try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Hero from '@/components/home/slider/Hero'
+import FeatureSection from '@/components/home/feature/FeatureSection'
 import BrowseCatagory from '@/components/home/browseCatagory/BrowseCatagory'
 import FeaturedProduct from '@/components/home/featuredProduct/FeaturedProduct'
 import ProductShowcase from '@/components/home/productShowcase/ProductShowcase'
 import DiscountProduct from '@/components/home/discountProduct/DiscountProduct'
 import CTA from '@/components/home/ctaSection/CTA'
-import Hero from '@/components/home/slider/Hero'
-import FeatureSection from '@/components/home/feature/FeatureSection'
 
 
 const Home = () => {
